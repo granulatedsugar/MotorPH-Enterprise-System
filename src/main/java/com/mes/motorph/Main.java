806a89392e38f1c70c7fc23a3484abe48058fead
Niro <48410720+granulatedsugar@users.noreply.github.com>
2024-02-15 22:41:22 -0800

    Dashboard-View
    Created initial dashboard view

--- src/main/java/com/mes/motorph/Main.java
+++ src/main/java/com/mes/motorph/Main.java
@@ -3,6 +3,7 @@ package com.mes.motorph;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +11,15 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("MotorPH Enterprise System");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("dashboard-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 768);
+        stage.setTitle("MotorPH Enterprise System V20240214");
         stage.setScene(scene);
+
+        // Load the application icon
+        Image icon = new Image(Main.class.getResourceAsStream("/images/app-icon.png"));
+        stage.getIcons().add(icon);
+
         stage.show();
     }
 
