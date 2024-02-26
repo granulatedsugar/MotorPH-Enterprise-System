69d7a8ee122d5f95944996ec09038b2ed9968373
Niro <48410720+granulatedsugar@users.noreply.github.com>
2024-02-25 22:50:06 -0800

    Refactor: PayrollCreateNew
    Refactor: PayrollCreateNew
    -Created Salary Calculation Service

--- src/main/java/com/mes/motorph/controller/PayrollCreateController.java
+++ src/main/java/com/mes/motorph/controller/PayrollCreateController.java
@@ -3,6 +3,7 @@ package com.mes.motorph.controller;
 import com.mes.motorph.entity.*;
 import com.mes.motorph.exception.*;
 import com.mes.motorph.services.*;
+import com.mes.motorph.utils.AlertUtility;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
@@ -26,7 +27,6 @@ public class PayrollCreateController {
     private Button calculateSalaryBtn;
     @FXML
     private TextField payrollIdField;
-    private String payrollId;
     @FXML
     private TextField employeeIdField;
     @FXML
@@ -68,76 +68,83 @@ public class PayrollCreateController {
     @FXML
     private Label netPayLabel;
 
+    private String payrollId;
+
     PayrollService payrollService = new PayrollService();
     DeductionService deductionService = new DeductionService();
     AttendanceService attendanceService = new AttendanceService();
     EmployeeService employeeService = new EmployeeService();
     PositionService positionService = new PositionService();
     DepartmentService departmentService = new DepartmentService();
+    SalaryCalculationService salaryCalculationService = new SalaryCalculationService();
 
     public void setPayrollId(String payrollId) {
         this.payrollId = payrollId;
         payrollIdField.setText(payrollId);
-
         breadCrumb.setText("Payroll / Update / Payslip #" + payrollId);
         payslipSceneTitle.setText("Update Payslip #" + payrollId);
 
         try {
             Payroll payrollEmployeeData = payrollService.fetchEmployeePayrollDetails(payrollId);
-
-            employeeIdField.setText(String.valueOf(payrollEmployeeData.getEmployeeId()));
-
-            // Date Picker for FROM AND TO
-            SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
-            Date fromDate = payrollEmployeeData.getPayPeriodFrom();
-            String formattedFromDate = formatter.format(fromDate);
-
-            // Convert to LocalDate for FROM AND TO
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-            LocalDate localFromDate = LocalDate.parse(formattedFromDate, dateTimeFormatter);
-            startDatePicker.setValue(localFromDate);
-
-            Date toDate = payrollEmployeeData.getPayPeriodTo();
-            String formattedToDate = formatter.format(toDate);
-            LocalDate localToDate = LocalDate.parse(formattedToDate, dateTimeFormatter);
-            endDatePicker.setValue(localToDate);
-
-            // Employee Information
-            employeeNameField.setText(payrollEmployeeData.getEmployeeName());
-            positionField.setText(payrollEmployeeData.getPosition());
-            deptField.setText(payrollEmployeeData.getDepartment());
-
-            // Earnings
-            monthlyRateField.setText(String.valueOf(payrollEmployeeData.getGrossSemiMonthlyrate() * 2));
-            double dailyRateCalc = (payrollEmployeeData.getGrossSemiMonthlyrate() * 2) / 20;
-            dailyRateField.setText(String.valueOf(dailyRateCalc));
-            daysWorkedField.setText(String.valueOf(payrollEmployeeData.getDaysWorked()));
-
-            // Deductions
-            sssField.setText(String.valueOf(payrollEmployeeData.getDeductionSss()));
-            phField.setText(String.valueOf(payrollEmployeeData.getDeductionPhilHealth()));
-            pagIbigField.setText(String.valueOf(payrollEmployeeData.getDeductionPagIbig()));
-            // Calculate Withholding
-            double withholdingTotal = deductionService.calculateWithholdingTax(dailyRateCalc * payrollEmployeeData.getDaysWorked());
-            // TODO : remove sout
-            System.out.println(withholdingTotal);
-            withholdingTaxField.setText(String.valueOf(withholdingTotal));
-
-            // Benefits
-            riceSubField.setText(String.valueOf(payrollEmployeeData.getAllowanceRice()));
-            phoneAllowanceField.setText(String.valueOf(payrollEmployeeData.getAllowancePhone()));
-            clothingAllowanceField.setText(String.valueOf(payrollEmployeeData.getAllowanceClothing()));
-
-            // Summary
-            grossIncomeLabel.setText(String.valueOf(payrollEmployeeData.getGrossPay()));
-            double totalDeductions = payrollEmployeeData.getDeductionSss() + payrollEmployeeData.getDeductionPagIbig() + payrollEmployeeData.getDeductionPhilHealth();
-            totalDeductionsLabel.setText(String.valueOf(totalDeductions));
-            double totalBenefits = payrollEmployeeData.getTotalAllowance() + payrollEmployeeData.getAllowanceRice() + payrollEmployeeData.getAllowancePhone();
-            totalBenefitsLabels.setText(String.valueOf(totalBenefits));
-            netPayLabel.setText(String.valueOf(payrollEmployeeData.getNetPay()));
+            setEmployeeDetails(payrollEmployeeData);
+            setPayPeriodDates(payrollEmployeeData);
+            setEarnings(payrollEmployeeData);
+            setDeductions(payrollEmployeeData);
+            setBenefitsAndSummary(payrollEmployeeData);
         } catch (PayrollException e) {
             e.printStackTrace();
         }
+    }
+
+    private void setEmployeeDetails(Payroll payrollEmployeeData) {
+        employeeIdField.setText(String.valueOf(payrollEmployeeData.getEmployeeId()));
+        employeeNameField.setText(payrollEmployeeData.getEmployeeName());
+        positionField.setText(payrollEmployeeData.getPosition());
+        deptField.setText(payrollEmployeeData.getDepartment());
+    }
+
+    private void setPayPeriodDates(Payroll payrollEmployeeData) {
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        String formattedFromDate = formatter.format(payrollEmployeeData.getPayPeriodFrom());
+        LocalDate localFromDate = LocalDate.parse(formattedFromDate, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+        startDatePicker.setValue(localFromDate);
+
+        String formattedToDate = formatter.format(payrollEmployeeData.getPayPeriodTo());
+        LocalDate localToDate = LocalDate.parse(formattedToDate, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+        endDatePicker.setValue(localToDate);
+    }
+
+    private void setEarnings(Payroll payrollEmployeeData) throws PayrollException {
+        double grossSemiMonthly = payrollEmployeeData.getGrossSemiMonthlyrate();
+        monthlyRateField.setText(String.valueOf(salaryCalculationService.calculateMonthlyRate(grossSemiMonthly)));
+        double dailyRateCalc = salaryCalculationService.calculateDailyRate(grossSemiMonthly);
+        dailyRateField.setText(String.valueOf(dailyRateCalc));
+        daysWorkedField.setText(String.valueOf(payrollEmployeeData.getDaysWorked()));
+    }
+
+    private void setDeductions(Payroll payrollEmployeeData) {
+        sssField.setText(String.valueOf(payrollEmployeeData.getDeductionSss()));
+        phField.setText(String.valueOf(payrollEmployeeData.getDeductionPhilHealth()));
+        pagIbigField.setText(String.valueOf(payrollEmployeeData.getDeductionPagIbig()));
+        double withholdingTotal = deductionService.calculateWithholdingTax(
+                (payrollEmployeeData.getGrossSemiMonthlyrate() * 2 / 20) * payrollEmployeeData.getDaysWorked()
+        );
+        withholdingTaxField.setText(String.valueOf(withholdingTotal));
+    }
+
+    private void setBenefitsAndSummary(Payroll payrollEmployeeData) {
+        riceSubField.setText(String.valueOf(payrollEmployeeData.getAllowanceRice()));
+        phoneAllowanceField.setText(String.valueOf(payrollEmployeeData.getAllowancePhone()));
+        clothingAllowanceField.setText(String.valueOf(payrollEmployeeData.getAllowanceClothing()));
+
+        double totalDeductions = payrollEmployeeData.getDeductionSss()
+                + payrollEmployeeData.getDeductionPagIbig()
+                + payrollEmployeeData.getDeductionPhilHealth();
+        totalDeductionsLabel.setText(String.valueOf(totalDeductions));
+        double totalBenefits = salaryCalculationService.calculateTotalBenefits(payrollEmployeeData.getAllowanceRice(), payrollEmployeeData.getAllowancePhone(),payrollEmployeeData.getTotalAllowance());
+        totalBenefitsLabels.setText(String.valueOf(totalBenefits));
+        grossIncomeLabel.setText(String.valueOf(payrollEmployeeData.getGrossPay()));
+        netPayLabel.setText(String.valueOf(payrollEmployeeData.getNetPay()));
     }
 
     // Override IF there is no attendance record for employee
@@ -149,6 +156,7 @@ public class PayrollCreateController {
         positionField.setDisable(!isSelected);
         deptField.setDisable(!isSelected);
         monthlyRateField.setDisable(!isSelected);
+        daysWorkedField.setDisable(!isSelected);
         riceSubField.setDisable(!isSelected);
         phoneAllowanceField.setDisable(!isSelected);
         clothingAllowanceField.setDisable(!isSelected);
@@ -156,77 +164,109 @@ public class PayrollCreateController {
     }
 
     @FXML
-    protected void onClickRunReport() throws AttendanceException, EmployeeException, PositionException, DepartmentException {
-
+    protected void onClickRunReport() {
         String eid = employeeIdField.getText();
 
-        Employee employee = employeeService.fetchEmployeeDetails(Integer.parseInt(eid));
-        List<Position> positions = positionService.fetchPositions(); // returns list of positions
-        List<Department> departments = departmentService.fetchDepartments();
+        try {
+            Employee employee = employeeService.fetchEmployeeDetails(Integer.parseInt(eid));
 
-        // Get Empployee Position
-        String employeePositionTitle = null;
-        for (Position position : positions) {
-            if (position.getPositionId() == employee.getPositionId()) {
-                employeePositionTitle = position.getTitle();
-                break;
-            }
+            // Update UI with employee information
+            updateEmployeeUI(employee);
+
+            // Calculate salary details
+            calculateSalaryDetails(employee);
+
+        } catch (EmployeeException e) {
+            e.printStackTrace();
+            // Handle exception
         }
+    }
 
-        // Get Employee Departments
-        String employeeDepartment = null;
-        for (Department department : departments) {
-            if (department.getDeptId() == employee.getDeptId()) {
-                employeeDepartment = department.getDeptDesc();
-                break;
+    private void updateEmployeeUI(Employee employee) {
+        try {
+            List<Position> positions = positionService.fetchPositions();
+            List<Department> departments = departmentService.fetchDepartments();
+
+            String employeePositionTitle = null;
+            for (Position position : positions) {
+                if (position.getPositionId() == employee.getPositionId()) {
+                    employeePositionTitle = position.getTitle();
+                    break;
+                }
             }
-        }
 
-        // Date
+            String employeeDepartment = null;
+            for (Department department : departments) {
+                if (department.getDeptId() == employee.getDeptId()) {
+                    employeeDepartment = department.getDeptDesc();
+                    break;
+                }
+            }
+
+            // Employee Information
+            employeeNameField.setText(employee.getFirstName() + " " + employee.getLastName());
+            positionField.setText(employeePositionTitle);
+            deptField.setText(employeeDepartment);
+        } catch (PositionException | DepartmentException e) {
+            e.printStackTrace();
+            // Handle exception
+        }
+    }
+
+    private void calculateSalaryDetails(Employee employee) {
         LocalDate selectedFromDate = startDatePicker.getValue();
         LocalDate selectedToDate = endDatePicker.getValue();
         DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         String fDate = selectedFromDate.format(outputFormatter);
         String tDate = selectedToDate.format(outputFormatter);
+        DateTimeFormatter payslipIdFormat = DateTimeFormatter.ofPattern("dd-yyyy-MM");
+        String payslipIdValue = selectedToDate.format(payslipIdFormat);
+        payrollIdField.setText(payslipIdValue + "-" + employee.getId());
 
-        List<Attendance> attendances = attendanceService.fetchAttendaceByEmployeId(Integer.parseInt(eid), fDate, tDate);
-        int attendanceCount = attendances.size();
+        try {
+            List<Attendance> attendances = attendanceService.fetchAttendaceByEmployeId(employee.getId(), fDate, tDate);
+            int attendanceCount = attendances.size();
 
-        // Emloyee Information
-        employeeNameField.setText(employee.getFirstName() + " " + employee.getLastName());
-        positionField.setText(employeePositionTitle);
-        deptField.setText(employeeDepartment);
+            // Earnings
+            double monthlyRate = salaryCalculationService.calculateMonthlyRate(employee.getGrossSemiMonthlyRate());
+            monthlyRateField.setText(String.valueOf(monthlyRate));
+            dailyRateField.setText(String.valueOf(monthlyRate / 20));
+            daysWorkedField.setText(String.valueOf(attendanceCount));
 
-        // Earnings
-        double monthlyRate = employee.getGrossSemiMonthlyRate() * 2;
-        monthlyRateField.setText(String.valueOf(monthlyRate));
-        dailyRateField.setText(String.valueOf(monthlyRate / 20));
-        daysWorkedField.setText(String.valueOf(attendanceCount));
+            // Benefits
+            double totalBenefits = salaryCalculationService.calculateTotalBenefits(employee.getRiceSubsidy(), employee.getPhoneAllowance(), employee.getClothingAllowance());
+            riceSubField.setText(String.valueOf(employee.getRiceSubsidy()));
+            phoneAllowanceField.setText(String.valueOf(employee.getPhoneAllowance()));
+            clothingAllowanceField.setText(String.valueOf(employee.getClothingAllowance()));
 
-        // Benefits
-        double totalBenefits = employee.getRiceSubsidy() + employee.getPhoneAllowance() + employee.getClothingAllowance();
-        riceSubField.setText(String.valueOf(employee.getRiceSubsidy()));
-        phoneAllowanceField.setText(String.valueOf(employee.getPhoneAllowance()));
-        clothingAllowanceField.setText(String.valueOf(employee.getClothingAllowance()));
-        totalBenefitsLabels.setText(String.valueOf(totalBenefits));
+            // Gross
+            double grossIncome = salaryCalculationService.calculateGrossIncome(monthlyRate, attendanceCount);
 
-        // Gross
-        double grossIncome = (monthlyRate / 20) * attendanceCount;
-        grossIncomeLabel.setText(String.valueOf(grossIncome));
+            if (attendanceCount != 0) {
+                // Deductions
+                double sssDeduction = deductionService.calculateSssDeduction(grossIncome);
+                double phDeduction = deductionService.calculatePhilHealthDeduction(grossIncome);
+                double pagIbiDeduction = deductionService.calculatePagIbigDeduction(grossIncome);
+                double withholdingTax = deductionService.calculateWithholdingTax(grossIncome);
+                double totalDeductions = sssDeduction + phDeduction + pagIbiDeduction + withholdingTax;
 
-        // Deductions
-        double sssDeduction = deductionService.calculateSssDeduction(grossIncome);
-        double phDeduction = deductionService.calculatePhilHealthDeduction(grossIncome);
-        double pagIbiDeduction = deductionService.calculatePagIbigDeduction(grossIncome);
-        double withholdingTax = deductionService.calculateWithholdingTax(grossIncome);
-        double totalDeductions = sssDeduction + phDeduction + pagIbiDeduction + withholdingTax;
+                sssField.setText(String.valueOf(sssDeduction));
+                phField.setText(String.valueOf(phDeduction));
+                pagIbigField.setText(String.valueOf(pagIbiDeduction));
+                withholdingTaxField.setText(String.valueOf(withholdingTax));
 
-        sssField.setText(String.valueOf(sssDeduction));
-        phField.setText(String.valueOf(phDeduction));
-        pagIbigField.setText(String.valueOf(pagIbiDeduction));
-        withholdingTaxField.setText(String.valueOf(withholdingTax));
-        totalDeductionsLabel.setText(String.valueOf(totalDeductions));
-
-        netPayLabel.setText(String.valueOf(grossIncome - totalDeductions + totalBenefits));
+                totalBenefitsLabels.setText(String.valueOf(totalBenefits));
+                grossIncomeLabel.setText(String.valueOf(grossIncome));
+                totalDeductionsLabel.setText(String.valueOf(totalDeductions));
+                double totalNetPay = salaryCalculationService.calculateNetPay(grossIncome, totalDeductions, totalBenefits);
+                netPayLabel.setText(String.valueOf(totalNetPay));
+            } else {
+                // Handle if no attendance data exists
+                AlertUtility.showAlert(Alert.AlertType.WARNING, "No Data", null, "No attendance found for " + employee.getFirstName() + " " + employee.getLastName());
+            }
+        } catch (AttendanceException | PayrollException e) {
+            e.printStackTrace();
+            // Handle exception
+        }
     }
 }
--- src/main/java/com/mes/motorph/services/SalaryCalculationService.java
+++ src/main/java/com/mes/motorph/services/SalaryCalculationService.java
@@ -1,6 +1,26 @@
 package com.mes.motorph.services;
 
+import com.mes.motorph.exception.PayrollException;
+
 public class SalaryCalculationService {
 
+    public double calculateMonthlyRate(double semiMonthlyRate) throws PayrollException {
+        return semiMonthlyRate * 2;
+    }
 
+    public double calculateDailyRate(double monthlyRate) throws PayrollException {
+        return monthlyRate / 20;
+    }
+
+    public double calculateTotalBenefits(double riceSubsidy, double phoneAllowance, double clothingAllowance) {
+        return riceSubsidy + phoneAllowance + clothingAllowance;
+    }
+
+    public double calculateGrossIncome(double monthlyRate, int daysWorked) {
+        return (monthlyRate / 20) * daysWorked;
+    }
+
+    public double calculateNetPay(double grossIncome, double totalDeductions, double totalBenefits) {
+        return grossIncome - totalDeductions + totalBenefits;
+    }
 }
